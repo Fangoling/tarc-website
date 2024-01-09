dd11e3a78f07b4c2d48704c82e911fd5a67e0252
fxliu <ge83toc@mytum.de>
2024-01-09 14:42:08 +0100

    added a navbar. todo title fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import Image from 'next/image'
-
+import './globals.css'
 export default function Home() {
   return (
     <main>
-
+      <section className='hero'>
+        <div className='content'>
+          <h1>TARC</h1>
+          <h1>Fitness</h1>
+          <p>The strongest gym in the world</p> 
+        </div>
+      </section>
     </main>
   )
 }
