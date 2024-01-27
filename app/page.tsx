b0a9f6a8e1cd3c90d43cd4b5b26793387fe42805
fxliu <ge83toc@mytum.de>
2024-01-27 17:33:12 +0100

    revamped the whole website

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,116 @@
-import './globals.css'
+import "./globals.css";
 export default function Home() {
   return (
-    <main>
-      <section className='hero'>
-        <div className='content'>
-          <h1>TARC</h1>
-          <h1>Fitness</h1>
-          <p>The strongest gym in the world</p> 
+    <body>
+      <header>
+        <img alt="Tarc Fitness" src="" />
+      </header>
+      <div className="header-video-container">
+        <img alt="tarc logo" src="/logos/tarc-fitness-logo.png" />
+      </div>
+      <div className="brand-grid-display">
+        <a className="square" href="" target="_blank">
+          <div className="grid-box coming-soon">
+            <img alt="Rotiform" src="/logos/coming-soon.svg" />
+          </div>
+        </a>
+        <a className="square" href="" target="_blank">
+          <div className="grid-box coming-soon">
+            <img alt="JP Insider" src="/logos/coming-soon.svg" />
+          </div>
+        </a>
+        <a className="square" href="" target="_blank">
+          <div className="grid-box coming-soon">
+            <img
+              alt="APR by JP Performance"
+              src="/logos/coming-soon.svg"
+            />
+          </div>
+        </a>
+        <a className="square" href="/" target="_blank">
+          <div className="grid-box coming-soon">
+            <img alt="Carffee" src="/logos/coming-soon.svg" />
+          </div>
+        </a>
+        <a className="square" href="" target="_blank">
+          <div className="grid-box coming-soon">
+            <img alt="Big Boost Burger" src="/logos/coming-soon.svg" />
+          </div>
+        </a>
+        <a className="square" href="" target="_blank">
+          <div className="grid-box coming-soon">
+            <img alt="JP Pace" src="/logos/coming-soon.svg" />
+          </div>
+        </a>
+        <a className="square" href="" target="_blank">
+          <div className="grid-box coming-soon">
+            <img alt="JP Clothing" src="/logos/coming-soon.svg" />
+          </div>
+        </a>
+        <a className="square" href="" target="_blank">
+          <div className="grid-box coming-soon">
+            <img alt="JP Superstore" src="/logos/coming-soon.svg" />
+          </div>
+        </a>
+        <a className="square" href="" target="_blank">
+          <div className="grid-box coming-soon">
+            <img alt="Time Attack" src="/logos/coming-soon.svg" />
+          </div>
+        </a>
+        <a className="square" href="" target="_blank">
+          <div className="grid-box coming-soon">
+            <img alt="JP Superstore" src="/logos/coming-soon.svg" />
+          </div>
+        </a>
+        <a className="square" href="" target="_blank">
+          <div className="grid-box coming-soon">
+            <img alt="JP Circle" src="/logos/coming-soon.svg" />
+          </div>
+        </a>
+        <a className="square" target="_blank">
+          <div className="grid-box coming-soon">
+            <img alt="Coming Soon" src="/logos/coming-soon.svg" />
+          </div>
+        </a>
+      </div>
+
+      <div className="information-flex-display">
+        <div className="information-contact">
+          <a href="mailto:Info@tarc-fitness.de" target="_blank">
+            <div className="contact-button-2">
+              <div className="top-line">Kontaktier uns</div>
+              <div className="bottom-line"></div>
+            </div>
+          </a>
         </div>
-      </section>
-    </main>
-  )
+        <div className="information-phone-number"></div>
+      </div>
+      <div className="social-media-icons">
+        <a href="" target="_blank">
+          <div className="facebook">
+            <img alt="Facebook" src="/icons/facebook-icon.svg" />
+          </div>
+        </a>
+        <a href="" target="_blank">
+          <div className="youtube">
+            <img alt="YouTube" src="/icons/youtube-icon.svg" />
+          </div>
+        </a>
+        <a href="https://instagram.com/tarc.fitness" target="_blank">
+          <div className="instagram">
+            <img alt="Instagram" src="/icons/instagram-icon.svg" />
+          </div>
+        </a>
+        <a href="" target="_blank">
+          <div className="tiktok">
+            <img alt="TikTok" src="/icons/tiktok-icon.svg" />
+          </div>
+        </a>
+      </div>
+      <div className="legal-navigation">
+        <a href="/impressum.html">Impressum</a>
+        <a href="/datenschutz.html">Datenschutz</a>
+      </div>
+    </body>
+  );
 }
